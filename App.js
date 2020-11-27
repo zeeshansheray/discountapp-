9d6ed964de5cc3dbd4b01784dc1d92effbed6fe4
Zeeshan Sheray <zeeshansheray1@gmail.com>
2020-11-28 01:58:15 +0500

    second commit

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [price,setPrice] = useState('');
-  const [discount,setDiscount] = useState('');
+  const [price, setPrice] = useState('');
+  const [discount, setDiscount] = useState('');
   const [priceInput, setpriceInput] = useState(false);
   const [discountInput, setdiscountInput] = useState(false);
+  const [priceResult, setPriceResult] = useState(null);
+  const [discountResult, setDiscountResult] = useState(null);
+  var totalDiscount = null;
+  var newPrice = null;
 
   const onclickButton = (num) => {
-    if(priceInput){
+    if (priceInput) {
       console.log('priceinput' + priceInput);
       setPrice(price.concat(num));
     }
-    else{
+    else {
       console.log('discount check' + discountInput)
       setDiscount(discount.concat(num));
     }
-}
+  }
+
+  const checkDiscount = () => {
+    newPrice = (price - (price * (discount / 100))).toFixed(2);
+    totalDiscount = (price - newPrice).toFixed(2);
+    setPriceResult(newPrice);
+    setDiscountResult(totalDiscount);
+    console.log("new price is " + priceResult);
+    console.log("discount is " + discountResult);
+
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.headingBox}>
         <Text style={styles.heading}>Discount Check</Text></View>
       <View style={styles.textInputBox}>
-  <Text style={styles.textInput}  onPress={()=>{setpriceInput(true),setdiscountInput(false)}} >{price== '' ? "Enter price" : price}</Text>
-  <Text style={styles.textInput}  onPress={()=>{setdiscountInput(true);setpriceInput(false)}}>{discount== '' ? "Enter discount %" : discount}</Text>
+        <Text style={styles.textInput} onPress={() => { setpriceInput(true), setdiscountInput(false) }} >{price == '' ? "Enter price" : price}</Text>
+        <Text style={styles.textInput} onPress={() => { setdiscountInput(true); setpriceInput(false) }}>{discount == '' ? "Enter discount %" : discount}</Text>
       </View>
       <View style={styles.viewstyle}>
         <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton("1") }} color="#001529" title="1" value="1" /></View>
@@ -34,24 +48,26 @@ export default function App() {
       </View>
       <View style={styles.viewstyle}>
         <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton("4") }} color="#001529" title="4" value="4" /></View>
-        <View style={{ width: '32%', marginRight: '1%' }} ><Button onPress={() => {onclickButton("5") }} color="#001529" title="5" value="5" /></View>
-        <View style={{ width: '32%' }}><Button onPress={() => {onclickButton("6") }} title="6" value="6" color="#001529" /></View>
+        <View style={{ width: '32%', marginRight: '1%' }} ><Button onPress={() => { onclickButton("5") }} color="#001529" title="5" value="5" /></View>
+        <View style={{ width: '32%' }}><Button onPress={() => { onclickButton("6") }} title="6" value="6" color="#001529" /></View>
       </View>
       <View style={styles.viewstyle}>
-        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => {onclickButton("7") }} color="#001529" title="7" value="7" /></View>
-        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => {onclickButton("8") }} color="#001529" title="8" value="8" /></View>
-        <View style={{ width: '32%' }}><Button onPress={() => {onclickButton("9") }} title="9" color="#001529" value="9" /></View>
+        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton("7") }} color="#001529" title="7" value="7" /></View>
+        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton("8") }} color="#001529" title="8" value="8" /></View>
+        <View style={{ width: '32%' }}><Button onPress={() => { onclickButton("9") }} title="9" color="#001529" value="9" /></View>
       </View>
       <View style={styles.viewstyle}>
-        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => {onclickButton(".") }} color="#001529" title="." value="." /></View>
-        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => {onclickButton("0") }} color="#001529" title="0" value="0" /></View>
-        <View style={{ width: '32%' }}><Button onPress={() => {priceInput ? setPrice(price.slice(0,-1)) : setDiscount(discount.slice(0,-1))}} title="del" color="#001529" value="del" /></View>
+        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton(".") }} color="#001529" title="." value="." /></View>
+        <View style={{ width: '32%', marginRight: '1%' }}><Button onPress={() => { onclickButton("0") }} color="#001529" title="0" value="0" /></View>
+        <View style={{ width: '32%' }}><Button onPress={() => { priceInput ? setPrice(price.slice(0, -1)) : setDiscount(discount.slice(0, -1)) }} title="del" color="#001529" value="del" /></View>
       </View>
       <View style={styles.viewstyle}>
         <View style={{ width: '30%', marginRight: '1%', marginLeft: '37%' }}>
-          <Button title="Discount" color="green" /></View>
+          <Button title="Discount" color="green" onPress={() => checkDiscount()} /></View>
         <View style={{ width: '30%', marginRight: '1%' }}><Button title="Save" color="darkblue" /></View>
       </View>
+      <View style={styles.resultBox}>
+        <Text style={styles.result}>{priceResult == null ? <View></View> : "Final Price: " + priceResult + ' Rs' + '\nYou saved: ' + discountResult + ' Rs'}</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -82,13 +98,13 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 27,
     padding: 10,
-    textAlign:'left',
+    textAlign: 'left',
   },
   textInputBox: {
     top: '25%',
     left: 80,
-   
-  }, 
+
+  },
   viewstyle: {
     flexDirection: 'row',
     top: '47%',
@@ -96,4 +112,13 @@ const styles = StyleSheet.create({
     left: '0.5%',
     height: '5%',
   },
+  resultBox: {
+    top: '31%',
+  },
+  result: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: '#001529',
+    textAlign: 'center'
+  }
 });
